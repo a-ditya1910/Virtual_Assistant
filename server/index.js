2a25f09af1bfb07907b58369f02b0e96a4ec8b43
adityada1905 <adityada1905@gmail.com>
2026-01-05 14:23:14 +0530

    updated url

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import userRouter from "./routes/user.routes.js"
 
 const app=express()
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://virtual-assistant-indol.vercel.app',
     credentials:true,
     methods: ["GET", "POST", "PUT", "DELETE"]
 }))
